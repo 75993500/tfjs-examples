6c8be89c79c2af2d659acedca6f31b9198f40ca6
jinxueqiang <jinxueqiang@nobook.com>
2018-12-22 17:34:26 +0800

    1

--- polynomial-regression-core/data.js
+++ polynomial-regression-core/data.js
@@ -1,20 +1,3 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 import * as tf from '@tensorflow/tfjs';
 
 export function generateData(numPoints, coeff, sigma = 0.04) {
@@ -27,6 +10,7 @@ export function generateData(numPoints, coeff, sigma = 0.04) {
     const xs = tf.randomUniform([numPoints], -1, 1);
 
     // Generate polynomial data
+    //   生成多项式数据
     const three = tf.scalar(3, 'int32');
     const ys = a.mul(xs.pow(three))
       .add(b.mul(xs.square()))
@@ -34,16 +18,18 @@ export function generateData(numPoints, coeff, sigma = 0.04) {
       .add(d)
       // Add random noise to the generated data
       // to make the problem a bit more interesting
+      // 将随机噪声添加到生成的数据中使问题更有趣
       .add(tf.randomNormal([numPoints], 0, sigma));
 
     // Normalize the y values to the range 0 to 1.
+    //   将y值标准化为0到1的范围。
     const ymin = ys.min();
     const ymax = ys.max();
     const yrange = ymax.sub(ymin);
     const ysNormalized = ys.sub(ymin).div(yrange);
 
     return {
-      xs, 
+      xs,
       ys: ysNormalized
     };
   })
--- polynomial-regression-core/index.js
+++ polynomial-regression-core/index.js
@@ -1,29 +1,15 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 import * as tf from '@tensorflow/tfjs';
 import {generateData} from './data';
 import {plotData, plotDataAndPredictions, renderCoefficients} from './ui';
 
 /**
+ * https://www.w3cschool.cn/tensorflowjs/tensorflowjs-y6kx2q2c.html
  * We want to learn the coefficients that give correct solutions to the
  * following cubic equation:
+ * 我们想要学习能够为其提供正确解决方案的系数以下方程式：
  *      y = a * x^3 + b * x^2 + c * x + d
  * In other words we want to learn values for:
+ *  换句话说，我们想要训练学习:
  *      a
  *      b
  *      c
@@ -32,11 +18,17 @@ import {plotData, plotDataAndPredictions, renderCoefficients} from './ui';
  * with x. We will provide some examples of 'xs' and 'ys' to allow this model
  * to learn what we mean by desired outputs and then use it to produce new
  * values of y that fit the curve implied by our example.
+ * 这样，当提供时，该函数为y产生“期望输出”用x。
+ * 我们将提供一些'xs'和'ys'的例子来允许这个模型通过期望的输出来学习我们的意思，
+ * 然后用它来产生新的符合我们示例所暗示的曲线的y值。
  */
 
 // Step 1. Set up variables, these are the things we want the model
 // to learn in order to do prediction accurately. We will initialize
 // them with random values.
+// 设置变量，这些是我们想要的模型学习以便准确地进行预测。 我们将初始化它们具有随机值。
+// 第1步：设置变量
+// 首先，我们需要创建一些变量。即开始我们是不知道a、b、c、d的值的，所以先给他们一个随机数，入戏所示：
 const a = tf.variable(tf.scalar(Math.random()));
 const b = tf.variable(tf.scalar(Math.random()));
 const c = tf.variable(tf.scalar(Math.random()));
@@ -45,24 +37,34 @@ const d = tf.variable(tf.scalar(Math.random()));
 
 // Step 2. Create an optimizer, we will use this later. You can play
 // with some of these values to see how the model performs.
+// 创建一个优化器，稍后我们将使用它。 你可以玩使用其中一些值来查看模型的执行情况。
+// SGD （Stochastic Gradient Descent）优化器，即随机梯度下降。SGD的工作原理就是利用数据中任意的点的梯度
+// 以及使用它们的值来决定增加或者减少我们模型中系数的值。
 const numIterations = 75;
-const learningRate = 0.5;
-const optimizer = tf.train.sgd(learningRate);
+// 学习率（learning rate）会控制模型调整幅度将会有多大。低的学习率会使得学习过程运行的更慢一些
+// （更多的训练迭代获得更符合数据的系数），而高的学习率将会加速学习过程但是将会导致最终的模型可能在正确值周围摇摆。
+// 简单的说，你既想要学的快，又想要学的好，这是不可能的。
+const learningRate = 0.5; // 学习率为0.5的SGD优化器
+const optimizer = tf.train.sgd(learningRate); // SGD 优化器
 
 // Step 3. Write our training process functions.
+// 写下我们的培训流程功能。
 
 /*
+ *  预测函数: 建立模型
  * This function represents our 'model'. Given an input 'x' it will try and
  * predict the appropriate output 'y'.
+ * 此功能代表我们的“模型”。 给定输入'x'将尝试和预测适当的输出'y'。
  *
  * It is also sometimes referred to as the 'forward' step of our training
  * process. Though we will use the same function for predictions later.
+ * 它有时也被称为我们培训的“前进”步骤处理。 虽然我们稍后会使用相同的功能进行预测。
  *
- * @return number predicted y value
+ * @return number predicted y value  数字预测y值
  */
 function predict(x) {
   // y = a * x ^ 3 + b * x ^ 2 + c * x + d
-  return tf.tidy(() => {
+  return tf.tidy(() => { // tidy函数进行内存管理,其作用:为执行一个函数并清除所有创建的中间张量
     return a.mul(x.pow(tf.scalar(3, 'int32')))
       .add(b.mul(x.square()))
       .add(c.mul(x))
@@ -71,42 +73,61 @@ function predict(x) {
 }
 
 /*
+定义损失函数
  * This will tell us how good the 'prediction' is given what we actually
  * expected.
+ * 这将告诉我们实际上给出的“预测”有多好预期。
  *
  * prediction is a tensor with our predicted y values.
  * labels is a tensor with the y values the model should have predicted.
+ * 预测是我们预测的y值的张量。标签是一个张量，具有模型应该预测的y值。
+ * 此处损失函数使用 MSE（均方误差，mean squared error）作为我们的损失函数
+ * MSE的计算非常简单，就是先根据给定的x得到实际的y值与预测得到的y值之差 的平方，然后在对这些差的平方求平均数即可。
+ * prediction: 预测值
+ * labels: 实际值
  */
 function loss(prediction, labels) {
   // Having a good error function is key for training a machine learning model
+  //   具有良好的错误功能是培训机器学习模型的关键
+  // 将labels（实际的值）进行抽象,然后获取平均数.
+  // square() 平方函数
+  // mean() 平均值函数
   const error = prediction.sub(labels).square().mean();
   return error;
 }
 
 /*
+ * 训练迭代器: 它会不断地运行SGD优化器来使不断修正、完善模型的系数来减小损失（MSE）
  * This will iteratively train our model.
+ * 这将迭代地训练我们的模型。
  *
- * xs - training data x values
- * ys — training data y values
+ * xs - training data x values x值
+ * ys — training data y values y值
+ * numIterations 制定的迭代次数
  */
 async function train(xs, ys, numIterations) {
-  for (let iter = 0; iter < numIterations; iter++) {
+  for (let iter = 0; iter < numIterations; iter++) { // 进行 numIterations 此迭代
     // optimizer.minimize is where the training happens.
+    //   optimizer.minimize是培训发生的地方。
 
     // The function it takes must return a numerical estimate (i.e. loss)
     // of how well we are doing using the current state of
     // the variables we created at the start.
+    //   它所需的功能必须返回一个数值估计（即损失）我们使用当前状态的表现如何我们在开始时创建的变量。
 
     // This optimizer does the 'backward' step of our training process
     // updating variables defined previously in order to minimize the
     // loss.
-    optimizer.minimize(() => {
+    //   此优化程序执行我们培训过程的“后退”步骤更新先前定义的变量以便最小化失利。
+    optimizer.minimize(() => { // 每次迭代调用optimizer优化器的minimize函数
       // Feed the examples into the model
+      //   将示例提供给模型
       const pred = predict(xs);
       return loss(pred, ys);
     });
 
     // Use tf.nextFrame to not block the browser.
+    //   使用tf.nextFrame不阻止浏览器。
     await tf.nextFrame();
   }
 }
@@ -116,10 +137,12 @@ async function learnCoefficients() {
   const trainingData = generateData(100, trueCoefficients);
 
   // Plot original data
+  //   绘制原始数据
   renderCoefficients('#data .coeff', trueCoefficients);
   await plotData('#data .plot', trainingData.xs, trainingData.ys)
 
   // See what the predictions look like with random coefficients
+  //   查看随机系数的预测结果
   renderCoefficients('#random .coeff', {
     a: a.dataSync()[0],
     b: b.dataSync()[0],
@@ -131,9 +154,11 @@ async function learnCoefficients() {
       '#random .plot', trainingData.xs, trainingData.ys, predictionsBefore);
 
   // Train the model!
+  //   训练模型！
   await train(trainingData.xs, trainingData.ys, numIterations);
 
   // See what the final results predictions are after training.
+  //   了解培训后的最终结果预测。
   renderCoefficients('#trained .coeff', {
     a: a.dataSync()[0],
     b: b.dataSync()[0],
